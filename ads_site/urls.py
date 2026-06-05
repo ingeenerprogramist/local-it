819feb92b6c8cdf6a49e34521b226420728c202c
Serik <antiriba@bk.ru>
2026-06-05 22:26:52 +0500

    fix urls include typo

--- ads_site/urls.py
+++ ads_site/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'), # главная страница
     path('about/', views.about, name='about'), # страница о нас
-    path('', include('ads_sit.urls')),
+    path('', include('ads_site.urls')),
 ]
 
